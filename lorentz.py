72556563dafe171c67d532b84be461c8344566c6
JamieGainer <jgainer137@gmail.con>
2019-12-13 23:10:52 -0800

    Removed explicit link to einstein_tensor

--- lorentz.py
+++ lorentz.py
@@ -1,8 +1,4 @@
-from __future__ import annotations
 
-import sys
-sys.path.append('/Users/jgainer/PMC/Physics/Utilities/einstein_tensor/einstein_tensor')
-import einstein_tensor as et
 
 class FourVector():
 
