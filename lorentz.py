c1474daea3b7f85fa8888f37f1be8542c8235991
JamieGainer <jgainer137@gmail.con>
2019-12-13 23:53:40 -0800

    Working on basic functionality for __init__.

--- lorentz.py
+++ lorentz.py
@@ -1,4 +1,13 @@
-
+import numpy as np
 
 class FourVector():
 
+    def __init__(self, E, p_x, p_y, p_z):
+        self.E, self.p_x, self.p_y, self.p_z = E, p_x, p_y, p_z
+        self.p_T = np.sqrt(self.p_x**2 + self.p_y**2)
+        self.p = np.sqrt(self.p_T**2 + self.p_z**2)
+        self.M = np.sqrt(self.E**2 - self.p**2)
+        self.phi = np.arctan2(self.p_y, self.p_x)
+        #self.theta =
+        #self.eta =
+        #self.phi =
--- test.py
+++ test.py
@@ -1,9 +1,36 @@
 import unittest
 import lorentz
 
+import numpy as np
+
 class TestFourVector(unittest.TestCase):
 
-    a = lorentz.FourVector([1, 0, 0, 0], '^mu', 'lab')
+    a = lorentz.FourVector(10., 3., 4., 5.)
+    def compare_phi_from_vector_with_given_phi_and_phi(self, phi):
+        a = lorentz.FourVector(10., 5. * np.cos(phi), 5. * np.sin(phi), 5.)
+        self.assertAlmostEqual(a.phi, phi)
+
+    def test_obtain_p_t(self):
+        self.assertEqual(self.a.p_T, 5.)
+
+    def test_obtain_M(self):
+        self.assertAlmostEqual(self.a.M, np.sqrt(50.))
+
+    def test_obtain_p(self):
+        self.assertAlmostEqual(self.a.p, np.sqrt(50.))
+
+    def test_obtain_phi_eq_phi_over_4(self):
+        self.compare_phi_from_vector_with_given_phi_and_phi(np.pi / 4)
+
+    def test_obtain_phi_eq_1(self):
+        self.compare_phi_from_vector_with_given_phi_and_phi(1.)
+
+    def test_obtain_phi_eq_3(self):
+        self.compare_phi_from_vector_with_given_phi_and_phi(3.)
+
+    def test_obtain_phi_eq_5(self):
+        self.compare_phi_from_vector_with_given_phi_and_phi(5.)
+
 
 if __name__ == '__main__':
     unittest.main()
