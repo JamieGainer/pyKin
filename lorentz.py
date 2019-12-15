956dc96817ad62a3641b1e4d1aa0b770e44dfd10
JamieGainer <jgainer137@gmail.con>
2019-12-14 17:05:32 -0800

    finished a functional FourVector class with unit tests.  Sketched an event class and methods to readin LHCO files.

--- lorentz.py
+++ lorentz.py
@@ -2,12 +2,94 @@ import numpy as np
 
 class FourVector():
 
-    def __init__(self, E, p_x, p_y, p_z):
+    def __init__(self, E, p_x, p_y, p_z, phi_range=(0, 2 * np.pi), theta_range=(0, np.pi)):
+        self._set_values(E, p_x, p_y, p_z, phi_range, theta_range)
+        self._set_p_pt_and_M()
+        self._obtain_phi()
+        self._obtain_theta_and_eta()
+
+    @classmethod
+    def from_eta_phi_pt_M(cls, eta, phi, p_T, M, phi_range=(0, 2 * np.pi), theta_range=(0, np.pi)):
+        p_x, p_y = [p_T * np.cos(phi), p_T * np.sin(phi)]
+        theta = 2. * np.arctan(np.exp(-eta))
+        p_z = p_T / np.tan(theta)
+        E = np.sqrt(M**2 + p_T**2 + p_z**2)
+        return FourVector(E, p_x, p_y, p_z, phi_range, theta_range)
+
+    def _set_values(self, E, p_x, p_y, p_z, phi_range, theta_range):
         self.E, self.p_x, self.p_y, self.p_z = E, p_x, p_y, p_z
+        self.phi_range, self.theta_range = phi_range, theta_range
+
+    def _set_p_pt_and_M(self):
         self.p_T = np.sqrt(self.p_x**2 + self.p_y**2)
         self.p = np.sqrt(self.p_T**2 + self.p_z**2)
         self.M = np.sqrt(self.E**2 - self.p**2)
+
+    def _obtain_phi(self):
         self.phi = np.arctan2(self.p_y, self.p_x)
-        #self.theta =
-        #self.eta =
-        #self.phi =
+        if not self._in(self.phi, self.phi_range):
+            self.phi -= np.floor(self.phi / (2 * np.pi)) * 2 * np.pi
+
+    def _obtain_theta_and_eta(self):
+        self.theta = np.arctan2(self.p_T, self.p_z)
+        self.eta = -np.log(np.tan(self.theta / 2))
+
+    def _in(self, value, range):
+        if value < range[0] or value > range[1]:
+            return False
+        return True
+
+# No test coverage below this line
+
+class LHCOEvent():
+
+    def __init__(self, line_string):
+        ints = [int(x) for x in line_string.split()]
+        try:
+            assert len(ints) == 3 and ints[0] == 0
+        except AssertionError:
+            raise ValueError("Not the first line of an event")
+        self.event_number, self.trigger_word = ints[1:]
+        self.object_dict = {object_type: [] for object_type in range(7)}
+
+    def add_line(self, line_string):
+        floats = [float(x) for x in line_string.split()]
+        try:
+            assert len(floats) == 11
+        except AssertionError:
+            raise ValueError("Not a valid physics object line")
+        number, object_type, eta, phi, p_T, jet_mass, n_tracks, b_tag, had_em, dummy_1, dummy_2 = floats
+        if object_type in [0, 1, 2]:
+            mass = {0: 0., 1: 0.000511, 2: 0.105}[object_type]
+        else:
+            mass = jet_mass
+        physics_object = FourVector.from_eta_phi_pt_M(eta, phi, p_T, mass)
+        (physics_object.number, physics_object.n_tracks,
+         physics_object.b_tag, physics_object.had_em,
+         physics_object.dummy_1, physics_object.dummy_2) = number, n_tracks, b_tag, had_em, dummy_1, dummy_2
+        self.object_dict[object_type].append(physics_object)
+
+    def finish_event(self):
+        for object_type, object_list in self.object_dict:
+            object_list.sort(key=lambda x: x.p_T, reverse=True)
+
+def lhco_event_list_from_file(file):
+    events = []
+    in_event = False
+    for line in file:
+        if in_event:
+            try:
+                events[-1].add_line(line)
+            except ValueError:
+                in_event = False
+                try:
+                    events.append(LHCOEvent(line))
+                    in_event = True
+                except ValueError:
+                    continue
+        else:
+            try:
+                events.append(LHCOEvent(line))
+                in_event = True
+            except ValueError:
+                continue
--- test.py
+++ test.py
@@ -5,11 +5,6 @@ import numpy as np
 
 class TestFourVector(unittest.TestCase):
 
-    a = lorentz.FourVector(10., 3., 4., 5.)
-    def compare_phi_from_vector_with_given_phi_and_phi(self, phi):
-        a = lorentz.FourVector(10., 5. * np.cos(phi), 5. * np.sin(phi), 5.)
-        self.assertAlmostEqual(a.phi, phi)
-
     def test_obtain_p_t(self):
         self.assertEqual(self.a.p_T, 5.)
 
@@ -19,17 +14,81 @@ class TestFourVector(unittest.TestCase):
     def test_obtain_p(self):
         self.assertAlmostEqual(self.a.p, np.sqrt(50.))
 
-    def test_obtain_phi_eq_phi_over_4(self):
-        self.compare_phi_from_vector_with_given_phi_and_phi(np.pi / 4)
-
     def test_obtain_phi_eq_1(self):
-        self.compare_phi_from_vector_with_given_phi_and_phi(1.)
+        self._compare_phi_from_vector_with_given_phi_and_phi(1.)
 
     def test_obtain_phi_eq_3(self):
-        self.compare_phi_from_vector_with_given_phi_and_phi(3.)
+        self._compare_phi_from_vector_with_given_phi_and_phi(3.)
 
     def test_obtain_phi_eq_5(self):
-        self.compare_phi_from_vector_with_given_phi_and_phi(5.)
+        self._compare_phi_from_vector_with_given_phi_and_phi(5.)
+
+    def test_obtain_theta_0_point_5(self):
+        self._compare_theta_from_vector_with_given_theta_and_phi_and_theta(0.5)
+
+    def test_obtain_theta_1(self):
+        self._compare_theta_from_vector_with_given_theta_and_phi_and_theta(1., phi=2.0)
+
+    def test_obtain_theta_2(self):
+        self._compare_theta_from_vector_with_given_theta_and_phi_and_theta(2., phi=4.0)
+
+    def test_obtain_eta_0(self):
+        self._compare_eta_from_vector_with_given_theta_and_phi_and_theta(np.pi / 2)
+
+    def test_obtain_eta_when_theta_eq_1(self):
+        self._compare_eta_from_vector_with_given_theta_and_phi_and_theta(1., phi=2.0)
+
+    def test_obtain_eta_when_theta_eq_2(self):
+        self._compare_theta_from_vector_with_given_theta_and_phi_and_theta(2., phi=4.0)
+
+    def test_define_four_vector_from_eta_phi_pt_M_when_theta_eq_0_point_5(self):
+        self._can_we_redefine_four_vector_from_eta_phi_pt_M(0.5, 1.)
+
+    def test_define_four_vector_from_eta_phi_pt_M_when_theta_eq_1(self):
+        self._can_we_redefine_four_vector_from_eta_phi_pt_M(1., 3.)
+
+    def test_define_four_vector_from_eta_phi_pt_M_when_theta_eq_2(self):
+        self._can_we_redefine_four_vector_from_eta_phi_pt_M(2., 3., E=13., p=2.)
+
+
+    a = lorentz.FourVector(10., 3., 4., 5.)
+
+    def assertListAlmostEqual(self, a, b):
+        self.assertEqual(len(a), len(b))
+        for a_i, b_i in zip(a, b):
+            self.assertAlmostEqual(a_i, b_i)
+
+    def _compare_phi_from_vector_with_given_phi_and_phi(self, phi):
+        a = lorentz.FourVector(10., 5. * np.cos(phi), 5. * np.sin(phi), 5.)
+        self.assertAlmostEqual(a.phi, phi)
+
+    def _compare_theta_from_vector_with_given_theta_and_phi_and_theta(self, theta, phi=1.):
+        a = lorentz.FourVector(10., 5. * np.cos(phi) * np.sin(theta),
+                               5. * np.sin(phi) * np.sin(theta),
+                               5. * np.cos(theta))
+        self.assertAlmostEqual(a.theta, theta)
+
+    def _compare_eta_from_vector_with_given_theta_and_phi_and_theta(self, theta, phi=1.):
+        a = lorentz.FourVector(10., 5. * np.cos(phi) * np.sin(theta),
+                               5. * np.sin(phi) * np.sin(theta),
+                               5. * np.cos(theta))
+        self.assertAlmostEqual(a.eta, self._eta_as_a_function_of_theta(theta))
+
+    def _eta_as_a_function_of_theta(self, theta):
+        return -np.log(np.tan(theta / 2))
+
+    def _generic_four_vector(self, theta, phi, E=10., p=5.):
+        return lorentz.FourVector(E, p * np.sin(theta) * np.cos(phi),
+                                  p * np.sin(theta) * np.sin(phi), p * np.cos(theta))
+
+    def _component_list_from_four_vector(self, four_vector):
+        return [four_vector.E, four_vector.p_x, four_vector.p_y, four_vector.p_z]
+
+    def _can_we_redefine_four_vector_from_eta_phi_pt_M(self, theta, phi, E=10., p=5.):
+        a = self._generic_four_vector(theta, phi, E=E, p=p)
+        b = lorentz.FourVector.from_eta_phi_pt_M(a.eta, a.phi, a.p_T, a.M)
+        self.assertListAlmostEqual(self._component_list_from_four_vector(a),
+                                   self._component_list_from_four_vector(b))
 
 
 if __name__ == '__main__':
