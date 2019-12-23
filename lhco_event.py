65b8010bc36ae05abb4051cef6578f2ad6fc3aee
JamieGainer <jgainer137@gmail.com>
2019-12-23 11:14:45 -0800

    Started class and tests for PhysicsObject

--- lhco_event.py
+++ lhco_event.py
@@ -1,7 +1,22 @@
+num_object_types = 7
+
+class PhysicsObject():
+
+    lhco_order_of_fields = ["object_number", "typ", "eta", "phi", "pt", "jmass", "ntrk", "btag",
+                            "had_em", "dummy1", "dummy2"]
+    possible_attributes = set(lhco_order_of_fields)
+
+    def __init__(self, **kwargs):
+        for attribute in kwargs:
+            if attribute in self.possible_attributes:
+                setattr(self, attribute, kwargs[attribute])
+
+
 class LHCOEvent():
 
     def __init__(self, event_number=None, trigger_word=None):
         self._set_event_properties(event_number, trigger_word)
+        self.object_register = {i: [] for i in range(num_object_types)}
 
     def _set_event_properties(self, event_number, trigger_word):
         if event_number:
--- test_lhco_event.py
+++ test_lhco_event.py
@@ -1,10 +1,63 @@
 import unittest
 import lhco_event
+import four_vector
+
+class TestPhysicsObject(unittest.TestCase):
+
+    lhco_object_string = "   1    2   -1.219  4.739   449.95   0.11   1.0   0.0    12.15   0.0   0.0 "
+
+    def test_physics_object_number(self):
+        new_physics_object = lhco_event.PhysicsObject(object_number=1)
+        self.assertEqual(new_physics_object.object_number, 1)
+
+    def test_physics_object_typ(self):
+        new_physics_object = lhco_event.PhysicsObject(typ=1)
+        self.assertEqual(new_physics_object.typ, 1)
+
+    def test_physics_object_eta(self):
+        new_physics_object = lhco_event.PhysicsObject(eta=1.0)
+        self.assertEqual(new_physics_object.eta, 1.0)
+
+    def test_physics_object_phi(self):
+        new_physics_object = lhco_event.PhysicsObject(phi=1)
+        self.assertEqual(new_physics_object.phi, 1.0)
+
+    def test_physics_object_pt(self):
+        new_physics_object = lhco_event.PhysicsObject(pt=1.0)
+        self.assertEqual(new_physics_object.pt, 1.0)
+
+    def test_physics_object_jmass(self):
+        new_physics_object = lhco_event.PhysicsObject(jmass=1.0)
+        self.assertEqual(new_physics_object.jmass, 1.0)
+
+    def test_physics_object_ntrk(self):
+        new_physics_object = lhco_event.PhysicsObject(ntrk=1.0)
+        self.assertEqual(new_physics_object.ntrk, 1.0)
+
+    def test_physics_object_btag(self):
+        new_physics_object = lhco_event.PhysicsObject(btag=1)
+        self.assertEqual(new_physics_object.btag, 1.0)
+
+    def test_physics_object_had_em(self):
+        new_physics_object = lhco_event.PhysicsObject(had_em=1.0)
+        self.assertEqual(new_physics_object.had_em, 1.0)
+
+    def test_physics_object_dummy1(self):
+        new_physics_object = lhco_event.PhysicsObject(dummy1=1.0)
+        self.assertEqual(new_physics_object.dummy1, 1.0)
+
+    def test_physics_object_dummy2(self):
+        new_physics_object = lhco_event.PhysicsObject(dummy2=1.0)
+        self.assertEqual(new_physics_object.dummy2, 1.0)
+
 
 class TestLHCOEvent(unittest.TestCase):
 
-    def test_create_event(self):
+    blank_object_register = {i: [] for i in range(lhco_event.num_object_types)}
+
+    def test_object_register_exists(self):
         new_event = lhco_event.LHCOEvent()
+        self.assertDictEqual(self.blank_object_register, new_event.object_register)
 
     def test_create_event_and_set_event_number(self):
         new_event = lhco_event.LHCOEvent(event_number=1)
