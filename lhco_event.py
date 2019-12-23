8d3868e53ddb0aff337fa8f54a3f6493afe1511b
JamieGainer <jgainer137@gmail.com>
2019-12-23 14:03:35 -0800

    added other potential attributes and named the list

--- lhco_event.py
+++ lhco_event.py
@@ -4,11 +4,12 @@ class PhysicsObject():
 
     lhco_order_of_fields = ["object_number", "type", "eta", "phi", "pt", "mass", "n_tracks", "btag",
                             "had_em", "dummy1", "dummy2"]
-    attributes_to_rename = {'jmass': 'mass', 'ntrk': 'n_tracks', 'e': 'energy'}
+    other_valid_fields = ['energy', 'px', 'py', 'pz', 'p', 'theta']
+    attributes_to_rename = {'jmass': 'mass', 'ntrk': 'n_tracks', 'e': 'energy', 'momentum': 'p'}
 
     def __init__(self, **kwargs):
         self.possible_attributes = set(self.lhco_order_of_fields)
-        self.possible_attributes.update(['energy', 'px', 'py', 'pz'])
+        self.possible_attributes.update(self.other_valid_fields)
         for attribute in kwargs:
             if attribute in self.possible_attributes:
                 setattr(self, attribute, kwargs[attribute])
