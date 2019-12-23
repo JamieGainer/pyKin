fa8250ca9312d82253f2d8ae03bb05196d9f3838
JamieGainer <jgainer137@gmail.com>
2019-12-23 09:33:14 -0800

    test and code to initialize with event number

--- lhco_event.py
+++ lhco_event.py
@@ -1,3 +1,5 @@
 class LHCOEvent():
 
-    pass
+    def __init__(self, event_number=None):
+        if event_number:
+            self.event_number = event_number
--- test_lhco_event.py
+++ test_lhco_event.py
@@ -6,6 +6,10 @@ class TestLHCOEvent(unittest.TestCase):
     def test_create_event(self):
         new_event = lhco_event.LHCOEvent()
 
+    def test_create_event_and_set_event_number(self):
+        new_event = lhco_event.LHCOEvent(event_number=1)
+        self.assertEqual(new_event.event_number, 1)
+
 
 if __name__ == '__main__':
     unittest.main()
