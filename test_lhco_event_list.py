81d637217ee5be64328c92770a1e3eb16bbd21a0
JamieGainer <jgainer137@gmail.com>
2020-01-04 12:05:25 -0800

    Refactored number of events test for lhco_event_list

--- test_lhco_event_list.py
+++ test_lhco_event_list.py
@@ -4,9 +4,11 @@ import lhco_event_list
 
 class TestLHCOEventList(unittest.TestCase):
 
-    def test_lhco_event_list_from_file(self):
-        event_list = lhco_event_list.LHCOEventList.from_file('demo.lhco')
-        self.assertEqual(len(event_list.list), 3)
+    event_list = lhco_event_list.LHCOEventList.from_file('demo.lhco')
+
+    def test_number_of_events_in_lhco_event_list_from_file(self):
+        self.assertEqual(len(self.event_list.list), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
