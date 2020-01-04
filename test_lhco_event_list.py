cbd8c1e9d80ddd6a6ecdd527a525505942fd7406
JamieGainer <jgainer137@gmail.com>
2020-01-04 12:15:47 -0800

    Added tests for number of remaining physics objects in each event in lhco_event_list

--- test_lhco_event_list.py
+++ test_lhco_event_list.py
@@ -18,6 +18,21 @@ class TestLHCOEventList(unittest.TestCase):
     def test_num_photons_lhco_event_list_from_file(self):
         self.assertListEqual([len(x.photons) for x in self.event_list.list], [0, 0, 0])
 
+    def test_num_electrons_lhco_event_list_from_file(self):
+        self.assertListEqual([len(x.electrons) for x in self.event_list.list], [0, 0, 2])
+
+    def test_num_muons_lhco_event_list_from_file(self):
+        self.assertListEqual([len(x.muons) for x in self.event_list.list], [1, 1, 0])
+
+    def test_num_taus_lhco_event_list_from_file(self):
+        self.assertListEqual([len(x.taus) for x in self.event_list.list], [0, 0, 0])
+
+    def test_num_jets_lhco_event_list_from_file(self):
+        self.assertListEqual([len(x.jets) for x in self.event_list.list], [2, 6, 5])
+
+    def test_num_stable_charged_particles_lhco_event_list_from_file(self):
+        self.assertListEqual([len(x.stable_charged_particles) for x in self.event_list.list], [0, 0, 0])
+
 
 if __name__ == '__main__':
     unittest.main()
