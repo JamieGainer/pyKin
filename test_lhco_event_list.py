022fad9a8a0acfcbc9d1539fc036ddf38316afd5
JamieGainer <jgainer137@gmail.com>
2020-01-04 12:07:22 -0800

    Added test of event numbers in lhco_event_list

--- test_lhco_event_list.py
+++ test_lhco_event_list.py
@@ -9,6 +9,9 @@ class TestLHCOEventList(unittest.TestCase):
     def test_number_of_events_in_lhco_event_list_from_file(self):
         self.assertEqual(len(self.event_list.list), 3)
 
+    def test_event_numbers_in_lhco_event_list_from_file(self):
+        self.assertListEqual([x.event_number for x in self.event_list.list], [103, 5, 3])
+
 
 if __name__ == '__main__':
     unittest.main()
