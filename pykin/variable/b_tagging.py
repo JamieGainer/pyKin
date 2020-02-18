4ac1a5df6096b6c372d5723862716adba6a9ee78
JamieGainer <jgainer137@gmail.con>
2020-02-17 21:28:53 -0800

    Added a sketch of b-tagging methods

---
+++ pykin/variable/b_tagging.py
@@ -0,0 +1,6 @@
+def get_b_tag_probabilities_from_jets_and_method(true_efficiency_function, fake_rate_function, jet_list):
+    pass
+
+# We'll define simple functions (of pT and eta) that give these efficiences...
+# and pass these functions to the above method in some convenience methods with
+# names like "CMS medium"
